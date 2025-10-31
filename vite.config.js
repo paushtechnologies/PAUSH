248317c251e56fc1c0007218f3e28637a394da6f
paushtechnologies <paushtechnologies@gmail.com>
2025-10-31 23:57:07 +0530

    deploy2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/PAUSH/", // <- important for GH Pages (repo name)
+  base: "/", // <- important for GH Pages (repo name)
   plugins: [react()],
 });
